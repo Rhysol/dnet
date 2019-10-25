6165297d6ed6076789eabc36d75c2031c9109186
Rhysol <rhysol@163.com>
2019-10-25 11:30:11 +0800

    untrack files in .gitignore

---
+++ CMakeFiles/net_lib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/net_lib.dir/src/main.cpp.o"
+  "CMakeFiles/net_lib.dir/src/listener.cpp.o"
+  "CMakeFiles/net_lib.dir/src/connection_manager.cpp.o"
+  "CMakeFiles/net_lib.dir/src/epoll_event_manager.cpp.o"
+  "CMakeFiles/net_lib.dir/src/io_event_pipe.cpp.o"
+  "CMakeFiles/net_lib.dir/src/io_thread.cpp.o"
+  "CMakeFiles/net_lib.dir/src/listener_thread.cpp.o"
+  "CMakeFiles/net_lib.dir/src/net_manager.cpp.o"
+  "net_lib.pdb"
+  "net_lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/net_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
